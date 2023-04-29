6a107d594b263c5012e8e377891e8fa38da68fa5
yayan <muhainun059@gmail.com>
2023-04-30 04:03:53 +0700

    install next js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blog.hubspot.com",
+        // port: '',
+        // pathname: '/account123/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
